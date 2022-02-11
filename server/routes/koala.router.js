eba31f43c97fa3ec8d864eb116ffd2dfd96923a6
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-11 09:37:50 -0600

    errors in code fixed, koala object successfully created, and connection made to database

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool');
 
 // DB CONNECTION
-
+const pg = require('pg');
+const Pool = pg.Pool;
+const config = {
+  database: 'koala_zone',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
+const pool = new Pool(config);
 
 // GET
 
