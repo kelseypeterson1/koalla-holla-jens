322a3172c83cd16abad6c8d8238dcdf7a467bb6b
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-11 10:52:27 -0600

    sending new koala data to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -57,5 +57,17 @@ function getKoalas() {
 function saveKoala(newKoala) {
   console.log('in saveKoala', newKoala);
   // ajax call to server to get koalas
+  $.ajax({
+      type: 'POST',
+      url: '/koalas',
+      data: newKoala
+  }).then( function (response) {
+      $('#nameIn').val(''),
+      $('#ageIn').val(''),
+      $('#genderIn').val(''),
+      $('#readyForTransferIn').val('')
+      $('#notesIn').val('')
+      getKoalas();
+  });
 
 }
